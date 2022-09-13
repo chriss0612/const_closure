032ded0d4cbb6f88304ce8d16d8ad701c19c4d45
onestacked <chrisi.schrefl@gmail.com>
2022-09-13 20:17:13 +0200

    Slight code improvements, formatting changes

--- src/lib.rs
+++ src/lib.rs
@@ -7,106 +7,112 @@
 
 #[macro_export]
 macro_rules! const_closure {
-    // Syntax sugar
-    (for $($end:tt)*) => {{
-        const_closure!(Fn for$($end)*)
-    }};
-    ($this:ident [$($mid:tt)*] $($end:tt)*) => {{
-        const_closure!(Fn for<> $this [$($mid)*] $($end)*)
-    }};
-    ($this:ident ($($mid:tt)*) $($end:tt)*) => {{
-        const_closure!(Fn for<> $this ($($mid)*) $($end)*)
-    }};
-    ($type:ident $this:ident [$($mid:tt)*] $($end:tt)*) => {{
-        const_closure!($type for<> $this [$($mid)*] $($end)*)
-    }};
-    ($type:ident for<$($gen_name:ident $(: $($trait_bound:path)*)?),*> $this:ident ($($mid:tt)*) $($end:tt)*) => {{
-        const_closure!($type for<$($gen_name $(: $($trait_bound)*)?),*> $this [] ($($mid)*) $($end)*)
-    }};
-    ($type:ident for<$($gen_name:ident $(: $($trait_bound:path)*)?),*> $this:ident [$($cap_name:ident : $cap_ty:ty ),* $(,)?] ($($($arg_name:ident: $arg_ty:ty),+)?) {$($body:tt)*}) => {{
-        const_closure!($type for<$($gen_name $(: $($trait_bound)*)?),*> $this [$($cap_name : $cap_ty ),*] ($($($arg_name: $arg_ty),+)?) -> () {$($body)*})
-    }};
-    // Actual Implementation
-    (FnOnce for<$($gen_name:ident $(: $($trait_bound:path)*)?),*> $this:ident [$($cap_name:ident : $cap_ty:ty ),+ $(,)?] ($($($arg_name:ident: $arg_ty:ty),+)?) -> $ret_ty:ty {$($body:tt)*}) => {{
-        #[allow(non_snake_case)]
-        struct Cl<$($gen_name $(: $($trait_bound)*)?),*> {
-            $($gen_name: ::core::marker::PhantomData<*const $gen_name>),*
-            $($cap_name: $cap_ty),*
-        }
-        impl<$($gen_name $(: $(~const $trait_bound + )+ ~const ::core::marker::Destruct)?),*> const FnOnce<($($($arg_ty),+,)?)> for Cl<$($gen_name),*> {
-            type Output = $ret_ty;
+  // Syntax sugar
+  (for $($end:tt)*) => {{
+    const_closure!(Fn for$($end)*)
+  }};
+  ($this:ident [$($mid:tt)*] $($end:tt)*) => {{
+    const_closure!(Fn for<> $this [$($mid)*] $($end)*)
+  }};
+  ($this:ident ($($mid:tt)*) $($end:tt)*) => {{
+    const_closure!(Fn for<> $this ($($mid)*) $($end)*)
+  }};
+  ($type:ident $this:ident [$($mid:tt)*] $($end:tt)*) => {{
+    const_closure!($type for<> $this [$($mid)*] $($end)*)
+  }};
+  ($type:ident for<$($gen_name:ident $(: $($trait_bound:path)*)?),*> $this:ident ($($mid:tt)*) $($end:tt)*) => {{
+    const_closure!($type for<$($gen_name $(: $($trait_bound)*)?),*> $this [] ($($mid)*) $($end)*)
+  }};
+  ($type:ident for<$($gen_name:ident $(: $($trait_bound:path)*)?),*> $this:ident [$($cap_name:ident : $cap_ty:ty ),* $(,)?] ($($arg_name:ident: $arg_ty:ty),*) {$($body:tt)*}) => {{
+    const_closure!($type for<$($gen_name $(: $($trait_bound)*)?),*> $this [$($cap_name : $cap_ty ),*] ($($arg_name: $arg_ty),*) -> () {$($body)*})
+  }};
+  // Actual Implementation
+  (FnOnce for<$($gen_name:ident $(: $($trait_bound:path)*)?),*> $this:ident [$($cap_name:ident : $cap_ty:ty ),+ $(,)?] ($($arg_name:ident: $arg_ty:ty),*) -> $ret_ty:ty {$($body:tt)*}) => {{
+    #[allow(non_snake_case)]
+    struct Cl<$($gen_name $(: $($trait_bound)*)?),*> {
+      $($gen_name: ::core::marker::PhantomData<*const $gen_name>),*
+      $($cap_name: $cap_ty),*
+    }
+    impl<$($gen_name $(: $(~const $trait_bound + )+ ~const ::core::marker::Destruct)?),*> const FnOnce<($($arg_ty,)*)> for Cl<$($gen_name),*> {
+      type Output = $ret_ty;
 
-            extern "rust-call" fn call_once(self, ($($($arg_name),+,)?): ($($($arg_ty),+,)?)) -> Self::Output {
-                #[allow(unused_mut)]
-                #[allow(unused_variables)]
-                let mut $this = self;
-                $($body)*
-            }
-        }
-        Cl {
-            $($gen_name: ::core::marker::PhantomData),*
-            $($cap_name: $cap_name),*
-        }
-    }};
-    (FnMut for<$($gen_name:ident $(: $($trait_bound:path)*)?),*> $this:ident [$($cap_name:ident : $cap_ty:ty),* $(,)?] ($($($arg_name:ident: $arg_ty:ty),+)?) -> $ret_ty:ty {$($body:tt)*}) => {{
-        #[allow(non_snake_case)]
-        struct Cl<'a, $($gen_name $(: $($trait_bound)*)?),*> {
-            _lt: ::core::marker::PhantomData<&'a mut u8>,
-            $($gen_name: ::core::marker::PhantomData<*const $gen_name>),*
-            $($cap_name: &'a mut $cap_ty),*
-        }
-        impl<'a, $($gen_name $(: $(~const $trait_bound + )+ ~const ::core::marker::Destruct)?),*> const FnOnce<($($($arg_ty),+,)?)> for Cl<'a, $($gen_name),*> {
-            type Output = $ret_ty;
+      #[allow(unused_parens)]
+      extern "rust-call" fn call_once(self, ($($arg_name,)*): ($($arg_ty,)*)) -> Self::Output {
+        #[allow(unused_mut)]
+        #[allow(unused_variables)]
+        let mut $this = self;
+        $($body)*
+      }
+    }
+    Cl {
+      $($gen_name: ::core::marker::PhantomData),*
+      $($cap_name: $cap_name),*
+    }
+  }};
+  (FnMut for<$($gen_name:ident $(: $($trait_bound:path)*)?),*> $this:ident [$($cap_name:ident : $cap_ty:ty),* $(,)?] ($($arg_name:ident: $arg_ty:ty),*) -> $ret_ty:ty {$($body:tt)*}) => {{
+    #[allow(non_snake_case)]
+    struct Cl<'a, $($gen_name $(: $($trait_bound)*)?),*> {
+      _lt: ::core::marker::PhantomData<&'a mut u8>,
+      $($gen_name: ::core::marker::PhantomData<*const $gen_name>),*
+      $($cap_name: &'a mut $cap_ty),*
+    }
+    impl<'a, $($gen_name $(: $(~const $trait_bound + )+ ~const ::core::marker::Destruct)?),*> const FnOnce<($($arg_ty,)*)> for Cl<'a, $($gen_name),*> {
+      type Output = $ret_ty;
 
-            extern "rust-call" fn call_once(mut self, args: ($($($arg_ty),+,)?)) -> Self::Output {
-                self.call_mut(args)
-            }
-        }
-        impl<'a, $($gen_name $(: $(~const $trait_bound + )+ ~const ::core::marker::Destruct)?),*> const FnMut<($($($arg_ty),+,)?)> for Cl<'a, $($gen_name),*> {
-            extern "rust-call" fn call_mut(&mut self, ($($($arg_name),+,)?): ($($($arg_ty),+,)?)) -> Self::Output {
-                #[allow(unused_variables)]
-                let $this = self;
-                $($body)*
-            }
-        }
-        Cl {
-            _lt: ::core::marker::PhantomData,
-            $($gen_name: ::core::marker::PhantomData),*
-            $($cap_name: &mut $cap_name),*
-        }
-    }};
-    (Fn for<$($gen_name:ident $(: $($trait_bound:path)*)?),*> $this:ident [$($cap_name:ident : $cap_ty:ty ),* $(,)?] ($($($arg_name:ident: $arg_ty:ty),+)?) -> $ret_ty:ty {$($body:tt)*}) => {{
-        #[allow(non_snake_case)]
-        struct Cl<'a, $($gen_name $(: $($trait_bound)*)?),*> {
-            _lt: ::core::marker::PhantomData<&'a u8>,
-            $($gen_name: ::core::marker::PhantomData<*const $gen_name>),*
-            $($cap_name: &'a $cap_ty),*
-        }
-        impl<'a, $($gen_name $(: $(~const $trait_bound + )+ ~const ::core::marker::Destruct)?),*> const FnOnce<($($($arg_ty),+,)?)> for Cl<'a, $($gen_name),*> {
-            type Output = $ret_ty;
+      #[allow(unused_parens)]
+      extern "rust-call" fn call_once(mut self, args: ($($arg_ty,)*)) -> Self::Output {
+        self.call_mut(args)
+      }
+    }
+    impl<'a, $($gen_name $(: $(~const $trait_bound + )+ ~const ::core::marker::Destruct)?),*> const FnMut<($($arg_ty,)*)> for Cl<'a, $($gen_name),*> {
 
-            extern "rust-call" fn call_once(mut self, args: ($($($arg_ty),+,)?)) -> Self::Output {
-                self.call_mut(args)
-            }
-        }
-        impl<'a, $($gen_name $(: $(~const $trait_bound + )+ ~const ::core::marker::Destruct)?),*> const FnMut<($($($arg_ty),+,)?)> for Cl<'a, $($gen_name),*> {
-            extern "rust-call" fn call_mut(&mut self, args: ($($($arg_ty),+,)?)) -> Self::Output {
-                self.call(args)
-            }
-        }
-        impl<'a, $($gen_name $(: $(~const $trait_bound + )+ ~const ::core::marker::Destruct)?),*> const Fn<($($($arg_ty),+,)?)> for Cl<'a, $($gen_name),*> {
-            extern "rust-call" fn call(&self, ($($($arg_name),+,)?): ($($($arg_ty),+,)?)) -> Self::Output {
-                #[allow(unused_variables)]
-                let $this = self;
-                $($body)*
-            }
-        }
-        Cl {
-            _lt: ::core::marker::PhantomData,
-            $($gen_name: ::core::marker::PhantomData),*
-            $($cap_name: &mut $cap_name),*
-        }
-    }};
+      #[allow(unused_parens)]
+      extern "rust-call" fn call_mut(&mut self, ($($arg_name,)*): ($($arg_ty,)*)) -> Self::Output {
+        #[allow(unused_variables)]
+        let $this = self;
+        $($body)*
+      }
+    }
+    Cl {
+      _lt: ::core::marker::PhantomData,
+      $($gen_name: ::core::marker::PhantomData),*
+      $($cap_name: &mut $cap_name),*
+    }
+  }};
+  (Fn for<$($gen_name:ident $(: $($trait_bound:path)*)?),*> $this:ident [$($cap_name:ident : $cap_ty:ty ),* $(,)?] ($($arg_name:ident: $arg_ty:ty),*) -> $ret_ty:ty {$($body:tt)*}) => {{
+    #[allow(non_snake_case)]
+    struct Cl<'a, $($gen_name $(: $($trait_bound)*)?),*> {
+      _lt: ::core::marker::PhantomData<&'a u8>,
+      $($gen_name: ::core::marker::PhantomData<*const $gen_name>),*
+      $($cap_name: &'a $cap_ty),*
+    }
+    impl<'a, $($gen_name $(: $(~const $trait_bound + )+ ~const ::core::marker::Destruct)?),*> const FnOnce<($($arg_ty,)*)> for Cl<'a, $($gen_name),*> {
+      type Output = $ret_ty;
+
+      extern "rust-call" fn call_once(mut self, args: ($($arg_ty,)*)) -> Self::Output {
+        self.call_mut(args)
+      }
+    }
+    impl<'a, $($gen_name $(: $(~const $trait_bound + )+ ~const ::core::marker::Destruct)?),*> const FnMut<($($arg_ty,)*)> for Cl<'a, $($gen_name),*> {
+      extern "rust-call" fn call_mut(&mut self, args: ($($arg_ty,)*)) -> Self::Output {
+        self.call(args)
+      }
+    }
+    impl<'a, $($gen_name $(: $(~const $trait_bound + )+ ~const ::core::marker::Destruct)?),*> const Fn<($($arg_ty,)*)> for Cl<'a, $($gen_name),*> {
+
+      #[allow(unused_parens)]
+      extern "rust-call" fn call(&self, ($($arg_name,)*): ($($arg_ty,)*)) -> Self::Output {
+        #[allow(unused_variables)]
+        let $this = self;
+        $($body)*
+      }
+    }
+    Cl {
+      _lt: ::core::marker::PhantomData,
+      $($gen_name: ::core::marker::PhantomData),*
+      $($cap_name: &mut $cap_name),*
+    }
+  }};
 }
 
 #[cfg(test)]
--- src/tests.rs
+++ src/tests.rs
@@ -2,64 +2,64 @@ use super::*;
 
 #[test]
 fn once_clusure() {
-    let x = 5;
-    let add1 = const_closure!(FnOnce this [x: i32] () {
-      this.x += 1;
-      assert!(this.x == 6);
-      ()
-    });
+  let x = 5;
+  let add1 = const_closure!(FnOnce this [x: i32] () {
+    this.x += 1;
+    assert!(this.x == 6);
+    ()
+  });
 
-    add1();
+  add1();
 
-    assert_eq!(x, 5);
+  assert_eq!(x, 5);
 
-    let add = const_closure!(FnOnce this [x: i32] (val1: i32) {
-      this.x += val1;
-      assert!(this.x == 10);
-      ()
-    });
+  let add = const_closure!(FnOnce this [x: i32] (val1: i32) {
+    this.x += val1;
+    assert!(this.x == 10);
+    ()
+  });
 
-    add(5);
-    assert_eq!(x, 5);
+  add(5);
+  assert_eq!(x, 5);
 }
 #[test]
 fn mut_clusure() {
-    let mut x = 5;
+  let mut x = 5;
 
-    let mut add1 = const_closure!(FnMut this [x: i32] () -> () {
-      *this.x += 1
-    });
+  let mut add1 = const_closure!(FnMut this [x: i32] () -> () {
+    *this.x += 1
+  });
 
-    add1();
+  add1();
 
-    assert_eq!(x, 6);
+  assert_eq!(x, 6);
 
-    let mut add = const_closure!(FnMut this [x: i32] (val1: i32) -> () {
-      *this.x += val1
-    });
+  let mut add = const_closure!(FnMut this [x: i32] (val1: i32) -> () {
+    *this.x += val1
+  });
 
-    add(5);
-    assert_eq!(x, 11);
+  add(5);
+  assert_eq!(x, 11);
 }
 #[test]
 fn fn_clusure() {
-    let mut x = 5;
+  let mut x = 5;
 
-    let add1 = const_closure!(this [x: i32] () -> i32 {
-      *this.x + 1
-    });
+  let add1 = const_closure!(this [x: i32] () -> i32 {
+    *this.x + 1
+  });
 
-    assert_eq!(add1(), 6);
-    assert!(x == 5);
+  assert_eq!(add1(), 6);
+  assert!(x == 5);
 
-    let add = const_closure!(this [x: i32] (val1: i32) -> i32 {
-      *this.x + val1
-    });
+  let add = const_closure!(this [x: i32] (val1: i32) -> i32 {
+    *this.x + val1
+  });
 
-    assert_eq!(add(5), 10);
-    assert!(x == 5);
-    let lt = const_closure!(for<T: PartialOrd> this (val1: T, val2: T) -> bool {
-      val1 < val2
-    });
-    assert_eq!(lt(5, 3), false)
+  assert_eq!(add(5), 10);
+  assert!(x == 5);
+  let lt = const_closure!(for<T: PartialOrd> this (val1: T, val2: T) -> bool {
+    val1 < val2
+  });
+  assert_eq!(lt(5, 3), false)
 }
